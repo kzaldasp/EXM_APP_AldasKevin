c020eec0938aaa41951ec6d760bc77574b51a13a
kzaldasp <kevinaldas290621@gmail.com>
2023-05-26 21:51:23 -0500

    Agregar archivos de proyecto.

---
+++ EXM_APP_AldasKevin/EXM_APP_AldasKevin/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EXM_APP_AldasKevin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
